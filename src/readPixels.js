28bbcfa542b34a065ec5b6471bacbfc4782e42a4
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-06-28 10:12:21 +0200

    0.3.3: readPixels is RGBA for better WebGL compat'

--- src/readPixels.js
+++ src/readPixels.js
@@ -1,9 +1,9 @@
 var ndarray = require("ndarray");
 
 function readPixels (gl, width, height) {
-  var pixels = new Uint8Array(width * height * 3);
-  gl.readPixels(0, 0, width, height, gl.RGB, gl.UNSIGNED_BYTE, pixels);
-  return ndarray(pixels, [ height, width, 3 ]);
+  var pixels = new Uint8Array(width * height * 4);
+  gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+  return ndarray(pixels, [ height, width, 4 ]);
 }
 
 module.exports = readPixels;
